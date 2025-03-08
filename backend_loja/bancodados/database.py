fb5656a067601b427523550b917f631561bac272
Thaysa Lima <thayprl01@gmail.com>
2025-03-08 20:56:00 -0300

    tabela de armazenamento de produtos.

--- backend_loja/bancodados/database.py
+++ backend_loja/bancodados/database.py
@@ -4,11 +4,22 @@ def criar_database():
     conector = sqlite3.connect("bancodados/acai_loja.db")
     cursor = conector.cursor()
 
+    #produtos
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS produtos (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             nome TEXT NOT NULL,
-            descricao TEXT,
-            preco
+            descricao TEXT, 
+
+            disponibilidade_balcao REAL DEFAULT 0.0,
+            unidade_balcao TEXT DEFAULT '',
+
+            disponibilidade_aberto REAL DEFAULT 0.0,
+            unidade_aberto TEXT DEFAULT '',
+
+            disponibilidade_lacrado REAL DEFAULT 0.0,
+            unidade_lacrado TEXT DEFAULT ''
         )
     ''')
+
+    
