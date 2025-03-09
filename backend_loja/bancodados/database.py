6440aacd3b2ec339b207526bfff5bb435882ea4b
Thaysa Lima <thayprl01@gmail.com>
2025-03-08 21:25:23 -0300

    mudei mais uma vez o código para deixar mais organizado e evitar os erros

--- backend_loja/bancodados/database.py
+++ backend_loja/bancodados/database.py
@@ -1,39 +1,52 @@
+import os
 import sqlite3
 
+def conectar_bd():
+    # Caminho da pasta onde o banco será salvo
+    pasta_bd = os.path.join(os.path.dirname(__file__), "bancodados")
+
+    if not os.path.exists(pasta_bd):
+        os.makedirs(pasta_bd)
+
+    caminho_db = os.path.join(pasta_bd, "acai_loja.db")
+
+    return sqlite3.connect(caminho_db)
+
+
 def criar_database():
-    conector = sqlite3.connect("bancodados/acai_loja.db")
+    conector = conectar_bd()
     cursor = conector.cursor()
 
-    #produtos
+    # Tabela de produtos gerais
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS produtos (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             nome TEXT NOT NULL,
             descricao TEXT, 
 
-            disponibilidade_balcao REAL DEFAULT 0.0,
+            estoque_balcao REAL DEFAULT 0.0,
             unidade_balcao TEXT DEFAULT '',
 
-            disponibilidade_aberto REAL DEFAULT 0.0,
+            estoque_aberto REAL DEFAULT 0.0,
             unidade_aberto TEXT DEFAULT '',
 
-            disponibilidade_lacrado REAL DEFAULT 0.0,
+            estoque_lacrado REAL DEFAULT 0.0,
             unidade_lacrado TEXT DEFAULT ''
         )
     ''')
 
-    #açaí
+    # Tabela específica para açaí
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS acai (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT NOT NULL,
-        descricao TEXT,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            descricao TEXT,
 
-        disponibilidade_balcao REAL DEFAULT 0.0,
-        unidade_balcao TEXT DEFAULT 'kg',
+            estoque_balcao REAL DEFAULT 0.0,
+            unidade_balcao TEXT DEFAULT 'kg',
 
-        disponibilidade_freezer REAL DEFAULT 0.0,
-        unidade_freezer TEXT DEFAULT 'kg'
+            estoque_freezer REAL DEFAULT 0.0,
+            unidade_freezer TEXT DEFAULT 'kg'
         )
     ''')
 
