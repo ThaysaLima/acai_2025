baab282fb9c25a5b2c27a34679062349f6ce62eb
Thaysa Lima <thayprl01@gmail.com>
2025-03-09 00:53:25 -0300

    definindo as rotas

---
+++ backend_loja/routes/produtos_routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, jsonify, request
+from bancodados.database import conectar_bd
+
+produtos_bp = Blueprint("produtos", __name__)
+
+#ROTAS:
+
+#rota para listar produtos 
+
+#rota para obter produto específico (ID)
+
+#rota para adicionar novo produto: 
+
